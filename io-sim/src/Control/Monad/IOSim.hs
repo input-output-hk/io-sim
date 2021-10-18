IntersectMBO/ouroboros-network

io-sim: added pretty printers
Pretty print 'Trace' and list of events (as returned by 'traceEvents').

io-sim/src/Control/Monad/IOSim.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -20,7 +21,11 @@ module Control.Monad.IOSim (
   unshareClock,
   -- * Simulation trace
   Trace,
+  ppTrace,
+  ppTrace',
+  ppEvents,
   Octopus (Trace, TraceMainReturn, TraceMainException, TraceDeadlock),
+  ppOctopus,
   Value(..),
   EventCtx(..),
   TraceEvent(..),
@@ -63,7 +68,7 @@ import           Control.Monad.Class.MonadThrow as MonadThrow
 import           Control.Monad.Class.MonadTime
 
 import           Control.Monad.IOSim.Internal
-import           Data.List.Octopus
+import           Data.List.Octopus (Octopus (..), ppOctopus)
 
 
 selectTraceEvents
@@ -240,6 +245,19 @@ traceEvents (Trace time tid tlbl event t) = (time, tid, tlbl, event)
                                           : traceEvents t
 traceEvents _                             = []
 
+ppEvents :: [(Time, ThreadId, Maybe ThreadLabel, TraceEvent)]
+         -> String
+ppEvents events =
+    intercalate "\n"
+      [ ppEventCtx width
+                   EventCtx {ecTime, ecThreadId, ecThreadLabel, ecTraceEvent }
+      | (ecTime, ecThreadId, ecThreadLabel, ecTraceEvent) <- events
+      ]
+  where
+    width = maximum
+              [ maybe 0 length threadLabel
+              | (_, _, threadLabel, _) <- events
+              ]
 
 
 -- | See 'runSimTraceST' below.

io-sim/src/Control/Monad/IOSim/Internal.hs
@@ -38,7 +38,11 @@ module Control.Monad.IOSim.Internal (
   ThreadId,
   ThreadLabel,
   Labelled (..),
+  -- * Simulation trace
   Trace,
+  ppTrace,
+  ppTrace',
+  ppEventCtx,
   Octopus (Trace, TraceMainReturn, TraceMainException, TraceDeadlock),
   EventCtx (..),
   Value (..),
@@ -49,19 +53,23 @@ module Control.Monad.IOSim.Internal (
 
 import           Prelude hiding (read)
 
+import           Data.Bifoldable
+import           Data.Bifunctor
 import           Data.Dynamic (Dynamic, toDyn)
 import           Data.Foldable (traverse_)
 import           Data.Function (on)
 import qualified Data.List as List
-import           Data.List.Octopus (Octopus (..))
+import           Data.List.Octopus (Octopus (..), ppOctopus)
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import           Data.Maybe (fromMaybe)
 import           Data.OrdPSQ (OrdPSQ)
 import qualified Data.OrdPSQ as PSQ
 import           Data.Set (Set)
 import qualified Data.Set as Set
 import           Data.Time (UTCTime (..), fromGregorian)
 import           Data.Typeable (Typeable)
+import           Text.Printf
 import           Quiet (Quiet (..))
 import           GHC.Generics (Generic)
 
@@ -594,11 +602,26 @@ labelledThreads threadMap =
 -- 'selectTraceEventsDynamic' and 'printTraceEventsSay'.
 --
 data EventCtx = EventCtx {
-    ecTime        :: !Time,
-    ecThreadId    :: !ThreadId,
-    ecThreadLabel :: !(Maybe ThreadLabel),
-    ecTraceEvent  :: !TraceEvent
+    ecTime        :: Time,
+    ecThreadId    :: ThreadId,
+    ecThreadLabel :: (Maybe ThreadLabel),
+    ecTraceEvent  :: TraceEvent
   }
+  deriving Generic
+  deriving Show via Quiet EventCtx
+
+ppEventCtx :: Int -- ^ width of thread label
+           -> EventCtx
+           -> String
+ppEventCtx d EventCtx {ecTime, ecThreadId, ecThreadLabel, ecTraceEvent} =
+    printf "%-24s - %-13s %-*s - %s"
+           (show ecTime)
+           (show ecThreadId)
+           d
+           threadLabel
+           (show ecTraceEvent)
+  where
+    threadLabel = fromMaybe "" ecThreadLabel
 
 data Value a
     = MainReturn    !Time a             ![Labelled ThreadId]
@@ -608,6 +631,20 @@ data Value a
 
 
 type Trace a = Octopus (Value a) EventCtx
+
+-- | Pretty print simulation trace.
+--
+ppTrace :: Show a => Trace a -> String
+ppTrace tr = ppOctopus show
+                       (ppEventCtx (bimaximum (bimap (const 0) (maybe 0 length . ecThreadLabel) tr)))
+                       tr
+
+-- | Like 'ppTrace' but does not show the result value.
+--
+ppTrace' :: Trace a -> String
+ppTrace' tr = ppOctopus (const "")
+                        (ppEventCtx (bimaximum (bimap (const 0) (maybe 0 length . ecThreadLabel) tr)))
+                        tr
 
 pattern Trace :: Time -> ThreadId -> Maybe ThreadLabel -> TraceEvent -> Trace a
               -> Trace a

input-output-hk/io-sim

io-sim/src/Control/Monad/IOSim.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ExplicitNamespaces  #-}
+{-# LANGUAGE NamedFieldPuns      #-}
 {-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -22,6 +23,9 @@ module Control.Monad.IOSim (
   -- * Simulation trace
   type SimTrace,
   Trace (Cons, Nil, Trace, SimTrace, TraceMainReturn, TraceMainException, TraceDeadlock),
+  ppTrace,
+  ppTrace_,
+  ppEvents,
   SimResult(..),
   SimEvent(..),
   SimEventType(..),
@@ -57,7 +61,7 @@ import           Data.List (intercalate)
 import           Data.Bifoldable
 import           Data.Typeable (Typeable)
 
-import           Data.List.Trace
+import           Data.List.Trace (Trace (..))
 
 import           Control.Exception (throw)
 
@@ -243,6 +247,19 @@ traceEvents (SimTrace time tid tlbl event t) = (time, tid, tlbl, event)
                                              : traceEvents t
 traceEvents _                                = []
 
+ppEvents :: [(Time, ThreadId, Maybe ThreadLabel, SimEventType)]
+         -> String
+ppEvents events =
+    intercalate "\n"
+      [ ppSimEvent width
+                   SimEvent {seTime, seThreadId, seThreadLabel, seType }
+      | (seTime, seThreadId, seThreadLabel, seType) <- events
+      ]
+  where
+    width = maximum
+              [ maybe 0 length threadLabel
+              | (_, _, threadLabel, _) <- events
+              ]
 
 
 -- | See 'runSimTraceST' below.

io-sim/src/Control/Monad/IOSim/Internal.hs
@@ -44,6 +44,9 @@ module Control.Monad.IOSim.Internal (
   SimResult (..),
   SimEventType (..),
   TraceEvent,
+  ppTrace,
+  ppTrace_,
+  ppSimEvent,
   liftST,
   execReadTVar
 
@@ -51,18 +54,22 @@ module Control.Monad.IOSim.Internal (
 
 import           Prelude hiding (read)
 
+import           Data.Bifoldable
+import           Data.Bifunctor
 import           Data.Dynamic (Dynamic, toDyn)
 import           Data.Foldable (traverse_)
 import qualified Data.List as List
 import qualified Data.List.Trace as Trace
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import           Data.Maybe (fromMaybe)
 import           Data.OrdPSQ (OrdPSQ)
 import qualified Data.OrdPSQ as PSQ
 import           Data.Set (Set)
 import qualified Data.Set as Set
 import           Data.Time (UTCTime (..), fromGregorian)
 import           Data.Typeable (Typeable)
+import           Text.Printf
 import           Quiet (Quiet (..))
 import           GHC.Generics (Generic)
 
@@ -600,6 +607,21 @@ data SimEvent = SimEvent {
     seThreadLabel :: !(Maybe ThreadLabel),
     seType        :: !SimEventType
   }
+  deriving Generic
+  deriving Show via Quiet SimEvent
+
+ppSimEvent :: Int -- ^ width of thread label
+           -> SimEvent
+           -> String
+ppSimEvent d SimEvent {seTime, seThreadId, seThreadLabel, seType} =
+    printf "%-24s - %-13s %-*s - %s"
+           (show seTime)
+           (show seThreadId)
+           d
+           threadLabel
+           (show seType)
+  where
+    threadLabel = fromMaybe "" seThreadLabel
 
 data SimResult a
     = MainReturn    !Time a             ![Labelled ThreadId]
@@ -609,6 +631,22 @@ data SimResult a
 
 
 type SimTrace a = Trace.Trace (SimResult a) SimEvent
+
+-- | Pretty print simulation trace.
+--
+ppTrace :: Show a => SimTrace a -> String
+ppTrace tr = Trace.ppTrace
+               show
+               (ppSimEvent (bimaximum (bimap (const 0) (maybe 0 length . seThreadLabel) tr)))
+               tr
+
+-- | Like 'ppTrace' but does not show the result value.
+--
+ppTrace_ :: SimTrace a -> String
+ppTrace_ tr = Trace.ppTrace
+                (const "")
+                (ppSimEvent (bimaximum (bimap (const 0) (maybe 0 length . seThreadLabel) tr)))
+                tr
 
 pattern Trace :: Time -> ThreadId -> Maybe ThreadLabel -> SimEventType -> SimTrace a
               -> SimTrace a