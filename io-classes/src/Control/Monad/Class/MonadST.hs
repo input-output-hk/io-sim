016c6a5016024c9c5f907a1f015ad8e880fe6fa0
Marcin Szamotulski <coot@coot.me>
2022-12-08 21:24:34 +0100

    MonadST: monad transformers instances

--- io-classes/src/Control/Monad/Class/MonadST.hs
+++ io-classes/src/Control/Monad/Class/MonadST.hs
@@ -1,7 +1,13 @@
 {-# LANGUAGE RankNTypes #-}
 module Control.Monad.Class.MonadST (MonadST (..)) where
 
+import           Control.Monad.Cont
+import           Control.Monad.Except
+import           Control.Monad.RWS
 import           Control.Monad.Reader
+import           Control.Monad.State
+import           Control.Monad.Writer
+
 import           Control.Monad.ST (ST, stToIO)
 
 
@@ -30,5 +36,20 @@ instance MonadST IO where
 instance MonadST (ST s) where
   withLiftST = \f -> f id
 
+instance MonadST m => MonadST (ContT r m) where
+  withLiftST f = withLiftST $ \g -> f (lift . g)
+
+instance MonadST m => MonadST (ExceptT e m) where
+  withLiftST f = withLiftST $ \g -> f (lift . g)
+
 instance MonadST m => MonadST (ReaderT r m) where
+  withLiftST f = withLiftST $ \g -> f (lift . g)
+
+instance (Monoid w, MonadST m) => MonadST (RWST r w s m) where
+  withLiftST f = withLiftST $ \g -> f (lift . g)
+
+instance MonadST m => MonadST (StateT s m) where
+  withLiftST f = withLiftST $ \g -> f (lift . g)
+
+instance (Monoid w, MonadST m) => MonadST (WriterT w m) where
   withLiftST f = withLiftST $ \g -> f (lift . g)
