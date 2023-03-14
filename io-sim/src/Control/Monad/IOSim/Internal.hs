8f712f4d9997fa77dafeb436863960aa6e9b7728
Marcin Szamotulski <coot@coot.me>
2023-03-14 09:54:00 +0100

    Removed not needed CPP extension

--- io-sim/src/Control/Monad/IOSim/Internal.hs
+++ io-sim/src/Control/Monad/IOSim/Internal.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE BangPatterns              #-}
-{-# LANGUAGE CPP                       #-}
 {-# LANGUAGE DerivingVia               #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleInstances         #-}
