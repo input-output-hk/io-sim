50f7b3e0cef1b8b94112ed82e4c0449c52d635aa
Marcin Szamotulski <coot@coot.me>
2023-03-14 09:54:00 +0100

    Removed not needed CPP extension

--- strict-stm/src/Control/Concurrent/Class/MonadSTM/Strict/TArray.hs
+++ strict-stm/src/Control/Concurrent/Class/MonadSTM/Strict/TArray.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE BangPatterns          #-}
-{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE ExplicitNamespaces    #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
