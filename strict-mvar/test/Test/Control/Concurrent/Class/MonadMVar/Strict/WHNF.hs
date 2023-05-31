a250ac0ee95a77f0918b7224f62775746dc557b0
Joris Dral <joris@well-typed.com>
2023-05-31 10:40:10 +0200

    Resolve PR comments: monomorphisation

--- strict-mvar/test/Test/Control/Concurrent/Class/MonadMVar/Strict/WHNF.hs
+++ strict-mvar/test/Test/Control/Concurrent/Class/MonadMVar/Strict/WHNF.hs
@@ -1,19 +1,19 @@
-{-# LANGUAGE CPP              #-}
-{-# LANGUAGE RankNTypes       #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE CPP #-}
 
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use camelCase" #-}
 
+-- | Test whether functions on 'StrictMVar's correctly force values to WHNF
+-- before they are put inside the 'StrictMVar'.
 module Test.Control.Concurrent.Class.MonadMVar.Strict.WHNF (tests) where
 
-#if CHECKED
+#if TEST_CHECKED
 import           Control.Concurrent.Class.MonadMVar.Strict.Checked
 #else
 import           Control.Concurrent.Class.MonadMVar.Strict
 #endif
 import           Control.Monad (void)
-import           Data.Typeable (Proxy (..), Typeable)
+import           Data.Typeable (Typeable)
 import           NoThunks.Class (OnlyCheckWhnf (OnlyCheckWhnf), unsafeNoThunks)
 import           Test.QuickCheck.Monadic (PropertyM, monadicIO, monitor, run)
 import           Test.Tasty (TestTree, testGroup)
@@ -26,7 +26,7 @@ import           Test.Utils (monadicSim)
 -------------------------------------------------------------------------------}
 
 name :: String
-#if CHECKED
+#if TEST_CHECKED
 name = "Strict.Checked"
 #else
 name = "Strict"
@@ -37,43 +37,43 @@ tests = testGroup ("Test.Control.Concurrent.Class.MonadMVar." <> name) [
       testGroup "WHNF" [
           testGroup "IO" [
               testProperty "prop_newMVar" $
-                monadicIO .: prop_newMVar (Proxy @Int)
+                monadicIO .: prop_newMVar
             , testProperty "prop_newMVarWithInvariant" $
-                monadicIO .: prop_newMVarWithInvariant (Proxy @Int)
+                monadicIO .: prop_newMVarWithInvariant
             , testProperty "prop_putMVar" $
-                monadicIO .: prop_putMVar (Proxy @Int)
+                monadicIO .: prop_putMVar
             , testProperty "prop_swapMVar" $
-                monadicIO .: prop_swapMVar (Proxy @Int)
+                monadicIO .: prop_swapMVar
             , testProperty "prop_tryPutMVar" $
-                monadicIO .: prop_tryPutMVar (Proxy @Int)
+                monadicIO .: prop_tryPutMVar
             , testProperty "prop_modifyMVar_" $
-                monadicIO .: prop_modifyMVar_ (Proxy @Int)
+                monadicIO .: prop_modifyMVar_
             , testProperty "prop_modifyMVar" $
-                monadicIO .: prop_modifyMVar (Proxy @Int) (Proxy @Char)
+                monadicIO .: prop_modifyMVar
             , testProperty "prop_modifyMVarMasked_" $
-                monadicIO .: prop_modifyMVarMasked_ (Proxy @Int)
+                monadicIO .: prop_modifyMVarMasked_
             , testProperty "prop_modifyMVarMasked" $
-                monadicIO .: prop_modifyMVarMasked (Proxy @Int) (Proxy @Char)
+                monadicIO .: prop_modifyMVarMasked
             ]
         , testGroup "IOSim" [
               testProperty "prop_newMVar" $ \x f ->
-                monadicSim $ prop_newMVar (Proxy @Int) x f
+                monadicSim $ prop_newMVar x f
             , testProperty "prop_newMVarWithInvariant" $ \x f ->
-                monadicSim $ prop_newMVarWithInvariant (Proxy @Int) x f
+                monadicSim $ prop_newMVarWithInvariant x f
             , testProperty "prop_putMVar" $ \x f ->
-                monadicSim $ prop_putMVar (Proxy @Int) x f
+                monadicSim $ prop_putMVar x f
             , testProperty "prop_swapMVar" $ \x f ->
-                monadicSim $ prop_swapMVar (Proxy @Int) x f
+                monadicSim $ prop_swapMVar x f
             , testProperty "prop_tryPutMVar" $ \x f ->
-                monadicSim $ prop_tryPutMVar (Proxy @Int) x f
+                monadicSim $ prop_tryPutMVar x f
             , testProperty "prop_modifyMVar_" $ \x f ->
-                monadicSim $ prop_modifyMVar_ (Proxy @Int) x f
+                monadicSim $ prop_modifyMVar_ x f
             , testProperty "prop_modifyMVar" $ \x f ->
-                monadicSim $ prop_modifyMVar (Proxy @Int) (Proxy @Char) x f
+                monadicSim $ prop_modifyMVar x f
             , testProperty "prop_modifyMVarMasked_" $ \x f ->
-                monadicSim $ prop_modifyMVarMasked_ (Proxy @Int) x f
+                monadicSim $ prop_modifyMVarMasked_ x f
             , testProperty "prop_modifyMVarMasked" $ \x f ->
-                monadicSim $ prop_modifyMVarMasked (Proxy @Int) (Proxy @Char) x f
+                monadicSim $ prop_modifyMVarMasked x f
             ]
         ]
     ]
@@ -90,10 +90,9 @@ infixr 9 .:
 isInWHNF :: (MonadMVar m, Typeable a) => StrictMVar m a -> PropertyM m Bool
 isInWHNF v = do
     x <- run $ readMVar v
-    let tinfoMay = unsafeNoThunks (OnlyCheckWhnf x)
-    case tinfoMay of
+    case unsafeNoThunks (OnlyCheckWhnf x) of
       Nothing    -> pure True
-      Just tinfo -> monitor (counterexample $ "Thunk found: " ++ show tinfo)
+      Just tinfo -> monitor (counterexample $ "Not in WHNF: " ++ show tinfo)
                  >> pure False
 
 {-------------------------------------------------------------------------------
@@ -101,101 +100,90 @@ isInWHNF v = do
 -------------------------------------------------------------------------------}
 
 prop_newMVar ::
-     (MonadMVar m, Typeable a)
-  => proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_newMVar _ x f = do
+prop_newMVar x f = do
     v <- run $ newMVar (applyFun f x)
     isInWHNF v
 
 prop_newMVarWithInvariant ::
-     (MonadMVar m, Typeable a)
-  => proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_newMVarWithInvariant _ x f = do
+prop_newMVarWithInvariant x f = do
     v <- run $ newMVarWithInvariant (const Nothing) (applyFun f x)
     isInWHNF v
 
 prop_putMVar ::
-     (MonadMVar m, Typeable a)
-  => proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_putMVar _ x f = do
+prop_putMVar x f = do
     v <- run newEmptyMVar
     run $ putMVar v (applyFun f x)
     isInWHNF v
 
 prop_swapMVar ::
-     (MonadMVar m, Typeable a)
-  => proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_swapMVar _ x f = do
+prop_swapMVar x f = do
     v <- run $ newMVar x
     void $ run $ swapMVar v (applyFun f x)
     isInWHNF v
 
 prop_tryPutMVar ::
-     (MonadMVar m, Typeable a)
-  => proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_tryPutMVar _ x f = do
+prop_tryPutMVar x f = do
     v <- run newEmptyMVar
     b <- run $ tryPutMVar v (applyFun f x)
     b' <- isInWHNF v
     pure (b && b')
 
 prop_modifyMVar_ ::
-     (MonadMVar m, Typeable a)
-  => Proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_modifyMVar_ _ x f =do
+prop_modifyMVar_ x f =do
     v <-  run $ newMVar x
     run $ modifyMVar_ v (pure . applyFun f)
     isInWHNF v
 
 prop_modifyMVar ::
-     (MonadMVar m, Typeable a)
-  => Proxy a
-  -> Proxy b
-  -> a
-  -> Fun a (a, b)
+     MonadMVar m
+  => Int
+  -> Fun Int (Int, Char)
   -> PropertyM m Bool
-prop_modifyMVar _ _ x f =do
+prop_modifyMVar x f =do
     v <-  run $ newMVar x
     void $ run $ modifyMVar v (pure . applyFun f)
     isInWHNF v
 
 prop_modifyMVarMasked_ ::
-     (MonadMVar m, Typeable a)
-  => Proxy a
-  -> a
-  -> Fun a a
+     MonadMVar m
+  => Int
+  -> Fun Int Int
   -> PropertyM m Bool
-prop_modifyMVarMasked_ _ x f =do
+prop_modifyMVarMasked_ x f =do
     v <-  run $ newMVar x
     void $ run $ modifyMVarMasked_ v (pure . applyFun f)
     isInWHNF v
 
 prop_modifyMVarMasked ::
-     (MonadMVar m, Typeable a)
-  => Proxy a
-  -> Proxy b
-  -> a
-  -> Fun a (a, b)
+     MonadMVar m
+  => Int
+  -> Fun Int (Int, Char)
   -> PropertyM m Bool
-prop_modifyMVarMasked _ _ x f =do
+prop_modifyMVarMasked x f =do
     v <-  run $ newMVar x
     void $ run $ modifyMVarMasked v (pure . applyFun f)
     isInWHNF v
