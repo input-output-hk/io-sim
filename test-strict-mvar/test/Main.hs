7f4fb90d4d799316217c53be8c0be8106182e3cd
Marcin Szamotulski <coot@coot.me>
2024-07-01 14:01:40 +0200

    Removed test-strict-mvar
    Move `IO` tests to `io-classes` and `IOSim` tests to `io-sim`.

--- test-strict-mvar/test/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Test.Control.Concurrent.Class.MonadMVar.Strict.WHNF qualified as WHNF
-import Test.Tasty
-
-main :: IO ()
-main = defaultMain $ testGroup "strict-mvar" [
-      WHNF.tests
-    ]
