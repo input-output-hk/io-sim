e705813a76e728002f587552e9f27b64db0128b0
Marcin Szamotulski <coot@coot.me>
2025-09-04 21:15:41 +0200

    check-stylish script updated

--- io-classes/io-classes/Control/Concurrent/Class/MonadSTM.hs
+++ io-classes/io-classes/Control/Concurrent/Class/MonadSTM.hs
@@ -1,14 +1,12 @@
 -- | This module corresponds to "Control.Concurrent.STM" in "stm" package
 --
-module Control.Concurrent.Class.MonadSTM
-  (module STM)
-  where
+module Control.Concurrent.Class.MonadSTM (module STM) where
 
-import Control.Monad.Class.MonadSTM              as STM
-import Control.Concurrent.Class.MonadSTM.TVar    as STM
-import Control.Concurrent.Class.MonadSTM.TMVar   as STM
-import Control.Concurrent.Class.MonadSTM.TChan   as STM
-import Control.Concurrent.Class.MonadSTM.TQueue  as STM
+import Control.Concurrent.Class.MonadSTM.TArray as STM
 import Control.Concurrent.Class.MonadSTM.TBQueue as STM
-import Control.Concurrent.Class.MonadSTM.TArray  as STM
+import Control.Concurrent.Class.MonadSTM.TChan as STM
+import Control.Concurrent.Class.MonadSTM.TMVar as STM
+import Control.Concurrent.Class.MonadSTM.TQueue as STM
+import Control.Concurrent.Class.MonadSTM.TVar as STM
+import Control.Monad.Class.MonadSTM as STM
 
--- scripts/check-stylish.sh
+++ scripts/check-stylish.sh
@@ -6,4 +6,4 @@ export LC_ALL=C.UTF-8
 [[ -x '/usr/bin/fd' ]] && FD="fd" ||  FD="fdfind"
 
 $FD . './io-sim' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
-$FD . './io-classes' -e hs -E Setup.hs -E src/Control/Concurrent/Class/MonadSTM.hs -X stylish-haskell -c .stylish-haskell.yaml -i
+$FD . './io-classes' -e hs -E Setup.hs -X stylish-haskell -c .stylish-haskell.yaml -i
